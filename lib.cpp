b26280779a1bce633949d9614c5dde35a0ed44be
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-12-15 15:50:36 -0300

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
 
+bool match(const char *ptr1, const char* ptr2)
+{
+    // zero bytes at ends
+    while (*ptr1 != 0 && *ptr2 != 0)
+    {
+        if (*ptr1 != *ptr2) {
+            return false;
+        }
+        ++ptr1;
+        ++ptr2;
+    }
+    return (*ptr1 == *ptr2);
+}
