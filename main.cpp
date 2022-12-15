33e300f46ab46dc740eaf94f077e1f9368864a40
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-12-15 15:58:00 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "lib.h"
 using namespace std;
 
 bool match(const char *ptr1, const char* ptr2);
@@ -8,20 +7,19 @@ int main (){
     char N[10][20];
     char C;
 
-    cout << "escreve os alunos" << endl;
 
     for (int i=0; i<10; i++) {
         cin >> N[i];
     }
-    cout << "escreve o alvo" << endl;
+    
     char M[0][20];
     cin >> M[0][20];
     for (int i=0; i<10; i++) {
         if (match(N[i], M[0])) {
-            cout << "presente" << endl;
+            cout << i << endl;
             break;
         } else
-            cout << "ausente" << endl;
+            cout << "non presente" << endl;
         break;
     }
 
